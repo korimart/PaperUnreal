611ba232b0212f0698b0d740eeb329ff3159322b
korimart <korimart2019@gmail.com>
2024-05-28 19:43:19 +0900

    init

---
+++ Source/PaperUnreal/PaperUnreal.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PaperUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PaperUnreal, "PaperUnreal" );
+
+DEFINE_LOG_CATEGORY(LogPaperUnreal)
+ 
---
+++ Source/PaperUnreal/PaperUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PaperUnrealGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APaperUnrealGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APaperUnrealGameMode();
+};
+
+
+
