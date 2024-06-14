2aa936a3748fa9b40760c0e1d11ddc01f952d2d4
korimart <korimart2019@gmail.com>
2024-06-14 17:08:58 +0900

    FPolygonBoundary2D 작성

--- Source/PaperUnreal/AreaMeshComponent.h
+++ Source/PaperUnreal/AreaMeshComponent.h
@@ -9,6 +9,148 @@
 #include "AreaMeshComponent.generated.h"
 
 
+/**
+ * counter-clockwise
+ */
+class FPolygonBoundary2D
+{
+public:
+	struct FIntersection
+	{
+		int32 SegmentStartIndex;
+		FVector2D ExactPosition;
+	};
+
+	bool IsValid() const
+	{
+		return Points.Num() >= 3;
+	}
+
+	bool IsInside(const FVector2D& Point) const
+	{
+		if (!IsValid())
+		{
+			return false;
+		}
+
+		const auto IsPointToTheLeftOfLine = [](
+			const FVector2D& PointOnLine0,
+			const FVector2D& PointOnLine1,
+			const FVector2D& Point)
+		{
+			const FVector2D Line = PointOnLine1 - PointOnLine0;
+			const FVector2D ToPoint = Point - PointOnLine0;
+			const float CrossProduct = FVector2D::CrossProduct(Line, ToPoint);
+			return CrossProduct < 0.f;
+		};
+
+		return AreAllOfSegments([&](const FVector2D& Start, const FVector2D& End)
+		{
+			return IsPointToTheLeftOfLine(Start, End, Point);
+		});
+	}
+
+	TOptional<FIntersection> DoesSegmentIntersect(const FVector2D& TargetSegmentStart, const FVector2D& TargetSegmentEnd) const
+	{
+		if (!IsValid())
+		{
+			return {};
+		}
+
+		FVector Intersection;
+		int32 Index = -1;
+		if (IsAnyOfSegments([&](const FVector2D& EachStart, const FVector2D& EachEnd)
+		{
+			Index++;
+			return FMath::SegmentIntersection2D(
+				FVector{TargetSegmentStart, 0.f},
+				FVector{TargetSegmentEnd, 0.f},
+				FVector{EachStart, 0.f},
+				FVector{EachEnd, 0.f},
+				Intersection);
+		}))
+		{
+			FIntersection Ret;
+			Ret.ExactPosition.X = Intersection.X;
+			Ret.ExactPosition.Y = Intersection.Y;
+			Ret.SegmentStartIndex = Index;
+			return Ret;
+		}
+
+		return {};
+	}
+
+	void Replace(int32 FirstIndex, int32 LastIndex, const TArray<FVector2D>& NewPoints)
+	{
+		const int32 Count = FMath::Min(LastIndex - FirstIndex + 1, Points.Num());
+		Points.RemoveAt(FirstIndex, Count);
+		Points.Insert(NewPoints, FirstIndex);
+	}
+
+	void SetSelfInDynamicMesh(UDynamicMesh* DynamicMesh) const
+	{
+		DynamicMesh->Reset();
+		
+		if (!IsValid())
+		{
+			return;
+		}
+
+		TArray<int32> PositionsToVertexIds;
+		bool bHasDuplicateVertices;
+		UGeometryScriptLibrary_MeshPrimitiveFunctions::AppendDelaunayTriangulation2D(
+			DynamicMesh,
+			{},
+			FTransform{FVector{0.f, 0.f, 100.f}},
+			Points,
+			{},
+			{},
+			PositionsToVertexIds,
+			bHasDuplicateVertices);
+	}
+
+private:
+	TArray<FVector2D> Points;
+
+	template <typename FuncType>
+	TOptional<int32> ForEachSegment(FuncType&& Func) const
+	{
+		for (int32 i = 0; i < Points.Num() - 1; i++)
+		{
+			if (!Func(Points[i], Points[i + 1]))
+			{
+				return i;
+			}
+		}
+
+		if (!Func(Points.Last(), Points[0]))
+		{
+			return Points.Num() - 1;
+		}
+
+		return {};
+	}
+
+	template <typename FuncType>
+	bool AreAllOfSegments(FuncType&& Func) const
+	{
+		return !ForEachSegment([&](const FVector2D& Start, const FVector2D& End)
+		{
+			return Func(Start, End);
+		});
+	}
+
+	template <typename FuncType>
+	bool IsAnyOfSegments(FuncType&& Func) const
+	{
+		return !!ForEachSegment([&](const FVector2D& Start, const FVector2D& End)
+		{
+			return !Func(Start, End);
+		});
+	}
+};
+
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UAreaMeshComponent : public UActorComponent
 {
@@ -17,6 +159,8 @@ class UAreaMeshComponent : public UActorComponent
 private:
 	UPROPERTY()
 	UDynamicMeshComponent* DynamicMeshComponent;
+
+	FPolygonBoundary2D AreaBoundary;
 
 	virtual void BeginPlay() override
 	{
@@ -38,34 +182,21 @@ private:
 
 	void SetCurrentAsStartingPoint()
 	{
-		const TArray<FVector2d> VertexPositions = [&]()
+		const TArray<FVector2D> VertexPositions = [&]()
 		{
-			TArray<FVector2d> Ret;
-
+			const FTransform Transform{FQuat{}, GetOwner()->GetActorLocation(), FVector{100.f}};
+			
+			TArray<FVector2D> Ret;
 			for (int32 AngleStep = 0; AngleStep < 16; AngleStep++)
 			{
 				const float ThisAngle = 2.f * UE_PI / 16.f * AngleStep;
-				Ret.Add({FMath::Cos(ThisAngle), FMath::Sin(ThisAngle)});
+				const FVector Vertex = Transform.TransformPosition(FVector{FMath::Cos(ThisAngle), FMath::Sin(ThisAngle), 0.f});
+				Ret.Add(FVector2D{Vertex});
 			}
-
 			return Ret;
 		}();
 
-		const FTransform Transform
-		{
-			FQuat{}, GetOwner()->GetActorLocation(), FVector{100.f}
-		};
-
-		TArray<int32> PositionsToVertexIds;
-		bool bHasDuplicateVertices;
-		UGeometryScriptLibrary_MeshPrimitiveFunctions::AppendDelaunayTriangulation2D(
-			DynamicMeshComponent->GetDynamicMesh(),
-			{},
-			Transform,
-			VertexPositions,
-			{},
-			{},
-			PositionsToVertexIds,
-			bHasDuplicateVertices);
+		AreaBoundary.Replace(0, 0, VertexPositions);
+		AreaBoundary.SetSelfInDynamicMesh(DynamicMeshComponent->GetDynamicMesh());
 	}
 };
