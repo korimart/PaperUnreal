8c8df37b6cd5be590a49b340afad292d33d0850a
korimart <korimart2019@gmail.com>
2024-07-18 11:38:40 +0900

    테스트 추가 작성

--- Source/PaperUnreal/Test/WeakCoroutineTest.cpp
+++ Source/PaperUnreal/Test/WeakCoroutineTest.cpp
@@ -23,9 +23,12 @@ bool FWeakCoroutineTest::RunTest(const FString& Parameters)
 		Array.Add(MakePromise<void>());
 		Array.Add(MakePromise<void>());
 		Array.Add(MakePromise<void>());
+		
+		auto Life = MakeUnique<FLife>();
+		auto bLifeDestroyed = Life->bDestroyed;
 
 		TArray<int32> Received;
-		RunWeakCoroutine2([&](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
+		RunWeakCoroutine2([&Array, &Received, Life = MoveTemp(Life)](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
 		{
 			co_await MoveTemp(Array[0].Get<1>());
 			Received.Add(0);
@@ -40,16 +43,22 @@ bool FWeakCoroutineTest::RunTest(const FString& Parameters)
 		});
 
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 0);
+		TestFalse(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[0].Get<0>().SetValue();
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 1);
+		TestFalse(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[1].Get<0>().SetValue();
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 2);
+		TestFalse(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[2].Get<0>().SetValue();
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 3);
+		TestFalse(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[3].Get<0>().SetValue();
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 4);
+		TestFalse(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[4].Get<0>().SetValue();
 		TestEqual(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 5);
+		TestTrue(TEXT("void Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 	}
 	
 	{
@@ -59,9 +68,12 @@ bool FWeakCoroutineTest::RunTest(const FString& Parameters)
 		Array.Add(MakePromise<int32>());
 		Array.Add(MakePromise<int32>());
 		Array.Add(MakePromise<int32>());
+		
+		auto Life = MakeUnique<FLife>();
+		auto bLifeDestroyed = Life->bDestroyed;
 
 		TArray<int32> Received;
-		RunWeakCoroutine2([&](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
+		RunWeakCoroutine2([&Array, &Received, Life = MoveTemp(Life)](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
 		{
 			Received.Add(co_await MoveTemp(Array[0].Get<1>()));
 			Received.Add(co_await MoveTemp(Array[1].Get<1>()));
@@ -71,16 +83,87 @@ bool FWeakCoroutineTest::RunTest(const FString& Parameters)
 		});
 
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received.Num(), 0);
+		TestFalse(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[0].Get<0>().SetValue(0);
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received[0], 0);
+		TestFalse(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[1].Get<0>().SetValue(1);
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received[1], 1);
+		TestFalse(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[2].Get<0>().SetValue(2);
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received[2], 2);
+		TestFalse(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[3].Get<0>().SetValue(3);
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received[3], 3);
+		TestFalse(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
 		Array[4].Get<0>().SetValue(4);
 		TestEqual(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), Received[4], 4);
+		TestTrue(TEXT("int32 Future를 잘 기다릴 수 있는지 테스트"), *bLifeDestroyed);
+	}
+
+	{
+		TArray<TTuple<TCancellablePromise<int32>, TCancellableFuture<int32>>> Array;
+		Array.Add(MakePromise<int32>());
+		Array.Add(MakePromise<int32>());
+		Array.Add(MakePromise<int32>());
+		Array.Add(MakePromise<int32>());
+		Array.Add(MakePromise<int32>());
+		
+		auto Life = MakeUnique<FLife>();
+		auto bLifeDestroyed = Life->bDestroyed;
+		auto AbortIfNotValid = MakeShared<bool>().ToSharedPtr();
+
+		TArray<int32> Received;
+		RunWeakCoroutine2([&Array, &AbortIfNotValid, &Received, Life = MoveTemp(Life)](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
+		{
+			Context.AbortIfNotValid(AbortIfNotValid);
+			
+			Received.Add(co_await MoveTemp(Array[0].Get<1>()));
+			Received.Add(co_await MoveTemp(Array[1].Get<1>()));
+			Received.Add(co_await MoveTemp(Array[2].Get<1>()));
+			Received.Add(co_await MoveTemp(Array[3].Get<1>()));
+			Received.Add(co_await MoveTemp(Array[4].Get<1>()));
+		});
+
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received.Num(), 0);
+		TestFalse(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+		Array[0].Get<0>().SetValue(0);
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received[0], 0);
+		TestFalse(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+		Array[1].Get<0>().SetValue(1);
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received[1], 1);
+		TestFalse(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+		
+		AbortIfNotValid = nullptr;
+		Array[2].Get<0>().SetValue(2);
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received.Num(), 2);
+		TestTrue(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+		Array[3].Get<0>().SetValue(3);
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received.Num(), 2);
+		TestTrue(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+		Array[4].Get<0>().SetValue(4);
+		TestEqual(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), Received.Num(), 2);
+		TestTrue(TEXT("AbortIfNotValid가 잘 작동하는는지 테스트"), *bLifeDestroyed);
+	}
+
+	{
+		TArray<TTuple<TCancellablePromise<int32>, TCancellableFuture<int32>>> Array;
+		Array.Add(MakePromise<int32>());
+		
+		auto Life = MakeUnique<FLife>();
+		auto bLifeDestroyed = Life->bDestroyed;
+		
+		TArray<int32> Received;
+		RunWeakCoroutine2([&Array, &Received, Life = MoveTemp(Life)](FWeakCoroutineContext2& Context) -> FWeakCoroutine2
+		{
+			Received.Add(co_await MoveTemp(Array[0].Get<1>()));
+		});
+
+		TestEqual(TEXT("약속이 지켜지지 않으면 종료하는지 테스트"), Received.Num(), 0);
+		TestFalse(TEXT("약속이 지켜지지 않으면 종료하는지 테스트"), *bLifeDestroyed);
+		Array.Empty();
+		TestEqual(TEXT("약속이 지켜지지 않으면 종료하는지 테스트"), Received.Num(), 0);
+		TestTrue(TEXT("약속이 지켜지지 않으면 종료하는지 테스트"), *bLifeDestroyed);
 	}
 	
 	return true;
