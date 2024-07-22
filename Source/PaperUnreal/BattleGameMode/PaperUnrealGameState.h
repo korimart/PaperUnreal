677c79688b7c54f6fcaff8d0478b2399c03ad9bd
korimart <korimart2019@gmail.com>
2024-07-22 14:45:21 +0900

    StageComponent 추가

--- Source/PaperUnreal/BattleGameMode/PaperUnrealGameState.h
+++ Source/PaperUnreal/BattleGameMode/PaperUnrealGameState.h
@@ -7,6 +7,7 @@
 #include "PaperUnreal/GameFramework2/GameStateBase2.h"
 #include "PaperUnreal/ModeAgnostic/PlayerSpawnerComponent.h"
 #include "PaperUnreal/ModeAgnostic/ReadyStateTrackerComponent.h"
+#include "PaperUnreal/ModeAgnostic/StageComponent.h"
 #include "PaperUnreal/ModeAgnostic/WorldTimerComponent.h"
 #include "PaperUnrealGameState.generated.h"
 
@@ -19,6 +20,9 @@ class APaperUnrealGameState : public AGameStateBase2
 	GENERATED_BODY()
 
 public:
+	UPROPERTY()
+	UStageComponent* StageComponent;
+	
 	UPROPERTY()
 	UWorldTimerComponent* WorldTimerComponent;
 	
@@ -34,6 +38,7 @@ public:
 private:
 	APaperUnrealGameState()
 	{
+		StageComponent = CreateDefaultSubobject<UStageComponent>(TEXT("StageComponent"));
 		WorldTimerComponent = CreateDefaultSubobject<UWorldTimerComponent>(TEXT("WorldTimerComponent"));
 		AreaSpawnerComponent = CreateDefaultSubobject<UAreaSpawnerComponent>(TEXT("AreaSpawnerComponent"));
 		PlayerSpawnerComponent = CreateDefaultSubobject<UPlayerSpawnerComponent>(TEXT("PlayerSpawnerComponent"));
---
+++ Source/PaperUnreal/ModeAgnostic/StageComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Net/UnrealNetwork.h"
+#include "PaperUnreal/GameFramework2/ActorComponent2.h"
+#include "PaperUnreal/WeakCoroutine/LiveData.h"
+#include "StageComponent.generated.h"
+
+
+UCLASS()
+class UStageComponent : public UActorComponent2
+{
+	GENERATED_BODY()
+
+public:
+	DECLARE_REPPED_LIVE_DATA_GETTER_SETTER(FName, CurrentStage, RepCurrentStage);
+
+private:
+	UPROPERTY(ReplicatedUsing=OnRep_CurrentStage)
+	FName RepCurrentStage;
+
+	UFUNCTION()
+	void OnRep_CurrentStage() { CurrentStage.OnRep(); }
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+		DOREPLIFETIME(ThisClass, RepCurrentStage);
+	}
+
+	UStageComponent()
+	{
+		SetIsReplicatedByDefault(true);
+	}
+};
